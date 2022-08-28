ad02015613bb2128b0efbf887094d31303a85b80
AlissonPeixer <alissonpeixer@pop-os.localdomain>
2022-08-28 20:29:42 -0300

    feat: created button(login event)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,17 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import Head from 'next/head';
+import type { NextPage } from 'next';
+import { signIn, signOut } from 'next-auth/react';
+
 
 const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Simple Auth | dev-0.2</title>
+        <title>Simple Auth | dev-0.3</title>
       </Head>
 
       <div className='container mx-auto h-screen flex items-center justify-center'>
-          
+          <button className='text-white' onClick={() => signIn('github')}>Login</button>
       </div>
 
     </>
