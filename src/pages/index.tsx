eb95c24118dbf85e4d7940479b6ea8ce28f4f0a5
Alisson Peixer <alissonpeixer4@gmail.com>
2022-09-02 14:53:38 -0300

    seed initial

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <>
+     <Head>
+        <title>NoteLis | dev-1.x</title>
+     </Head>
+     <div className='container h-screen flex items-center justify-center shadow-xl'>
+          <h1 className='text-center text-7xl'>Frist commit</h1>
+     </div>
+    </>
+  )
+}
+
+export default Home
